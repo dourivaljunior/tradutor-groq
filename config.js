ce09acc0ddd4e808b6523873437467639e53ba5d
Prof. Dourival Júnior <100372396+dourivaljunior@users.noreply.github.com>
2026-05-14 16:30:56 -0300

    Update GROQ_API_KEY in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ==================================================
 // CONFIGURAÇÕES SECRETAS - NÃO COMMITAR NO GITHUB
 // ==================================================
-const GROQ_API_KEY = "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";
+const GROQ_API_KEY = "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";
